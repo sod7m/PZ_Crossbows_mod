193d6ec7d8cc23a2e4d9e3692b91c507cb4d610a
sod7m <dmytrofeedback@gmail.com>
2026-07-14 03:15:35 +0300

    Remove unused ammo strap texture files: AmmoStrapBullets_Brown.png, AmmoStrapShells_Brown.png, AmmoStrap_Bullets.png, and AmmoStrap_Shells.png.

--- work/InWork/check_body_y.py
+++
@@ -1,9 +0,0 @@
-import sys, os
-sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
-from parse_x_to_obj import parse_mesh
-body = parse_mesh("E:/PZCrossbows/work/reference/models/MaleBody.x")
-verts = [(ox, oz, oy) for (ox, oy, oz) in body["verts"]]
-cand = [v for v in verts if 0.70 < v[2] < 0.95 and -0.05 < v[0] < 0.15]
-cand.sort(key=lambda v: -v[1])
-for v in cand[:15]:
-    print(v)
